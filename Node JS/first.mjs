e710d6ec50432ac1c3a04cd15a64687c11b313b0
Faizan Ahmed <67678975+ChFaizan009@users.noreply.github.com>
2021-11-19 19:39:22 +0500

    My First Update Before Study

---
+++ Node JS/first.mjs
@@ -0,0 +1,14 @@
+
+
+
+//this is EC Script import export process.
+
+export function al()
+{
+    console.log("This Course is for Aritifical Intellience")
+
+}
+export default function db()
+{
+    console.log("This Content is for DataBase.")
+}
